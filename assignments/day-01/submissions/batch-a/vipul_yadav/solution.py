1744d0bc2fd99977438ce8f564aad7b9f77414f2
Vipul Yadav <vipulyadav6068@gmail.com>
2026-06-13 22:58:58 +0530

    Add Day 1 assignment by Vipul Yadav

---
+++ assignments/day-01/submissions/batch-a/vipul_yadav/solution.py
@@ -0,0 +1,40 @@
+#  1. Print your name, age, and college name.
+
+print(" Name = Vipul Yadav \n age = 20 \n Collage = BIT")
+
+
+
+#2. Create variables for name, age, marks, and city.
+
+name = "Vipul Yadav"
+age = 20
+marks = 85
+city = "Gorakhpur"
+
+
+# 3. Use these data types: string, integer, float, and boolean.
+
+name = "Vipul Yadav"
+age = 20
+marks = 85.5
+is_student = True
+
+
+# 4. Create a list of 5 numbers and print the list.
+
+points = [2,5,4,8,9]
+
+for point in points:
+    print(point)
+
+
+# 5. Create a dictionary with name, course, city, and batch.
+
+students = {
+    "name":" Vipul Yadav ",
+    "city":" Gorakhpur ", 
+    "course":" Btech ", 
+    "Batch":"2024 - 2028"
+    }
+
+    
